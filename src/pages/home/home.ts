eaf9039061ce624423295d6b0cf75b393d382326
JAYANT8\jayantr <jayantr@mindfiresolutions.com>
2017-04-10 16:30:16 +0530

    initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+//import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  name1 = '';
+  name2 = '';
+  get score(){
+    if(this.name1.trim() == '' || this.name2.trim() == '')
+    {
+      return 0;
+    }
+    else
+    {
+      const letters = (this.name1.trim() + this.name2.trim()).toLowerCase();
+      let sum = 0;
+      for(let i = 0;i < letters.length;i++){
+        sum += letters.charCodeAt(i);
+      }
+      return sum % 101;
+    }
+  }
+  // constructor(public navCtrl: NavController) {
+    
+  // }
+
+}
